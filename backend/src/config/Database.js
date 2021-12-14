946a29efc175c41fe39fcfa8cdbb821d46658898
ramdhanll <erdevlop@gmail.com>
2021-12-15 00:01:51 +0700

    edit databases

--- backend/src/config/Database.js
+++ backend/src/config/Database.js
@@ -1,14 +1,18 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
 const Mongoose = () =>
    mongoose
-      .connect(process.env.MONGODB_URI || "mongodb://localhost/laporan-kerusakan-mesin", {
-         useUnifiedTopology: true,
-         useNewUrlParser: true,
-         useFindAndModify: true,
-         useCreateIndex: true,
-      })
-      .then(() => console.log("DB Connected"))
+      .connect(
+         process.env.MONGODB_URI ||
+            'mongodb://127.0.0.1:27017/laporan-kerusakan-mesin',
+         {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useFindAndModify: true,
+            useCreateIndex: true,
+         }
+      )
+      .then(() => console.log('DB Connected'))
       .catch((e) => console.log(e))
 
 export default Mongoose
